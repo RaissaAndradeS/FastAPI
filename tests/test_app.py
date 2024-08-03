f78c6950934821e6f6f77d2d0dba1f202f1905e9
RaissaAndradeS <221035077@aluno.unb.br>
2024-08-03 14:43:52 -0300

    Configurações do ambiente

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,16 @@
+from http import HTTPStatus
+
 from fastapi.testeclient import TesteClient
 
 from fast_zero.app import app
 
 client = TesteClient(app)
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TesteClient(app)  # Organização (Arrange)
+
+    response = client.get('/')  # Ação (Act)
+
+    assert response.status_code == HTTPStatus.OK  # Afirmando (Assert)
+    assert response.json() == {'message': 'Olá mundo'}
