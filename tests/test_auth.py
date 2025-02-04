1264a93c2e7ae7a3e1751193589c6fe863ebc7a6
RaissaAndradeS <221035077@aluno.unb.br>
2025-02-04 08:56:19 -0300

    Factory boy

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
+
 
 def test_get_token(client, user):
     response = client.post(
@@ -8,7 +10,80 @@ def test_get_token(client, user):
     )
 
     token = response.json()
-
     assert response.status_code == HTTPStatus.OK
     assert token['token_type'] == 'Bearer'
     assert 'acess_token' in token
+
+
+def test_token_expired_after_time(client, user):
+    with freeze_time('2024-12-08 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2024-12-08 12:31:00'):
+        response = client.put(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': 'wrongwrong',
+                'email': 'wrong@wrong.con',
+                'password': 'wrong',
+            },
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'datail': 'Cloud not validate  credentials'}
+
+
+def test_token_wrong_password(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': user.email, 'password': 'wrong_password'},
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'datail': 'Incorrect email or password'}
+
+
+def test_token_wrong_email(client, user):
+    response = client.post(
+        '/auth/token',
+        data={'username': 'bla@bla.com', 'password': user.clean_password},
+    )
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {'datail': 'Incorrect email or password'}
+
+
+def test_refresh_token(client, token):
+    response = client.post(
+        '/auth/refresh_token',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    data = response.json()
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in data
+    assert 'token_type' in data
+    assert data['token_type'] == 'bearer'
+
+
+def test_token_expired_dont_refresh(client, user):
+    with freeze_time('2024-12-08 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2024-12-08 12:31:00'):
+        response = client.post(
+            '/auth/refresh_token',
+            headers={'Authorization': f'Bearer {token}'},
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'datail': 'Cloud not validate  credentials'}
