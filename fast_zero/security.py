a4b36850e22cd0bb7ac38fb07e4d96db31125981
RaissaAndradeS <221035077@aluno.unb.br>
2025-02-17 10:29:38 -0300

    Arrumando tests

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -3,8 +3,7 @@ from http import HTTPStatus
 
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import decode, encode
-from jwt.exceptions import ExpiredSignatureError, PyJWKError
+from jwt import DecodeError, ExpiredSignatureError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -14,9 +13,20 @@ from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.settings import Settings
 
-pwd_context = PasswordHash.recommended()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 settings = Settings()
+pwd_context = PasswordHash.recommended()
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({'exp': expire})
+    encoded_jwt = encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
 
 
 def get_password_hash(password: str):
@@ -27,19 +37,7 @@ def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
-
-    to_encode.update({'exp': expire})
-    encoded_jwt = encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )
-
-    return encoded_jwt
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 def get_current_user(
@@ -51,21 +49,23 @@ def get_current_user(
         detail='Could not validate credentials',
         headers={'WWW-Authenticate': 'Bearer'},
     )
+
     try:
         payload = decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
-        username: str = payload.get('sub')
-        if not username:
+        subject_email = payload.get('sub')
+
+        if not subject_email:
             raise credentials_exception
+
+    except DecodeError:
+        raise credentials_exception
 
     except ExpiredSignatureError:
         raise credentials_exception
 
-    except PyJWKError:
-        raise credentials_exception
-
-    user = session.scalar(select(User).where(User.email == username))
+    user = session.scalar(select(User).where(User.email == subject_email))
 
     if not user:
         raise credentials_exception
